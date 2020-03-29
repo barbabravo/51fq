cb514ebd1122a25a2e4675c891284bbd94c8e2f1
biao.zhang <biao.zhang@goldrock.cn>
2020-03-29 11:39:32 +0800

    init

---
+++ initvpn.sh
@@ -0,0 +1,6 @@
+sudo apt-get update
+sudo apt-get -y install python-pip 
+sudo pip install git+https://github.com/shadowsocks/shadowsocks.git@master
+sudo cp shadowsocks.json /etc/shadowsocks.json
+sudo ssserver -c /etc/shadowsocks.json -d start
+sudo echo `/bin/ps axu | grep ssserver | grep -v grep`
