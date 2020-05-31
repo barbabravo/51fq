d881973ebbe546f8f065f70eb25519fe89ae4245
biao.zhang <biao.zhang@goldrock.cn>
2020-05-31 11:14:17 +0800

    fix: fix local error

--- initvpn.sh
+++ initvpn.sh
@@ -1,5 +1,6 @@
 sudo apt-get update
 sudo apt-get -y install python-pip 
+sudo export LC_ALL=C
 sudo pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 sudo cp shadowsocks.json /etc/shadowsocks.json
 sudo ssserver -c /etc/shadowsocks.json -d start
