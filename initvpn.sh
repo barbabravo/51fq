4bb7bcb34736b5f853b861a5e2aa2fcd1cf4c3cb
biao.zhang <biao.zhang@goldrock.cn>
2021-01-13 11:36:52 +0800

    update: local fix

--- initvpn.sh
+++ initvpn.sh
@@ -3,5 +3,6 @@ sudo apt-get -y install python-pip
 sudo export LC_ALL=C
 sudo pip install git+https://github.com/shadowsocks/shadowsocks.git@master
 sudo cp shadowsocks.json /etc/shadowsocks.json
+sudo apt install -y language-pack-zh-hans
 sudo ssserver -c /etc/shadowsocks.json -d start
 sudo echo `/bin/ps axu | grep ssserver | grep -v grep`
